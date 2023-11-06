e16f4562155a8b16005159bcbbea21027745495e
Ubuntu <ubuntu@ip-10-0-0-115.us-west-2.compute.internal>
2023-11-06 06:53:01 +0000

    Updates

---
+++ precache-manifest.88b4890b8f24e7f3bc5d70fcb68f236a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "22fb0f82549b4ed0a271",
+    "url": "/static/js/main.22fb0f82.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "d3933b46c81e0d1d8435b13a8b9e69af",
+    "url": "/index.html"
+  }
+];
